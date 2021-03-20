15dabeb5e9e049f10b191aad9be4cfe2383ae292
alexxstevens <41077253+alexxstevens@users.noreply.github.com>
2021-03-19 19:34:14 -0500

    alignment fixed

--- Components/Themes.js
+++ Components/Themes.js
@@ -1,5 +1,5 @@
-import React, {Component, useState} from 'react';
-import {Button, View, Text} from "react-native";
+import React, {Component} from 'react';
+import {Button, View} from "react-native";
 import getStyleSheet from './styles';
 
 export default class App extends Component {
@@ -16,13 +16,27 @@ export default class App extends Component {
   render(){
     const styles = getStyleSheet(this.state.Theme);
   return(
+    <>
     <View style={styles.container}>
       <View style={styles.box}>
         <Button title="dark" onPress={() => {this.setState({Theme: "DarkTheme"}); console.log(this.state);}} />
-        <Button title="light" onPress={() => {this.setState({Theme: "LightTheme"}); console.log(this.state);}} />
-        <Button title="patty" onPress={() => {this.setState({Theme: "PattyTheme"}); console.log(this.state);}} />
-        <Button title="sunshine" onPress={() => {this.setState({Theme: "SunshineTheme"}); console.log(this.state);}} />
       </View>
     </View>
+    <View style={styles.container}>
+      <View style={styles.box}>
+        <Button title="patty" onPress={() => {this.setState({Theme: "PattyTheme"}); console.log(this.state);}} />
+      </View>
+    </View>
+    <View style={styles.container}>
+      <View style={styles.box}>
+        <Button title="halloween" onPress={() => {this.setState({Theme: "HalloweenTheme"}); console.log(this.state);}} />   
+      </View>
+    </View>
+    <View style={styles.container}>
+      <View style={styles.box}>
+        <Button title="sunshine" onPress={() => {this.setState({Theme: "SunshineTheme"}); console.log(this.state);}} />  
+      </View>
+    </View>
+    </>
   )
   }};
